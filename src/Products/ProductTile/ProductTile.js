8e8a2fdfbbe9f2d1afa7f8684a1368c6594d9f49
Omanshu <omanshu.gupta@aricent.com>
2019-05-21 11:37:29 +0530

    Product tile template implemented with dynamic data passed by json file.

--- src/Products/ProductTile/ProductTile.js
+++ src/Products/ProductTile/ProductTile.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react'
 import './ProductTile.css'
+import axios from 'axios';
+
 class ProductTile extends Component {
+    state = {
+        products: []
+    }
+
+    componentWillMount() {
+        axios.get('/res/products.json')
+            .then(res => {
+                const products = res.data;
+                console.log(products)
+                this.setState({ products });
+            })
+    }
+
+    // productsRender (products) {
+    //     return ({products.map((product) => <div className="product">
+    //         <a href="#">
+    //             <img src={product.ImgUrl} width="100" height="100" /></a>
+    //         <div className="product-description">
+    //             Product Name : {product.Name}
+    //             <br />Brand : {product.Brand}
+    //             <br />Price : {product.Price}
+    //         </div>)})
+    //     }
 
     render() {
+        const productRender = this.state.products.map(product => <div className="product"><a href="#">
+                <img src={product.ImgUrl} width="100" height="100" /></a>
+            <div className="product-description">
+                Product Name : {product.Name}
+                <br />Brand : {product.Brand}
+                <br />Price : {product.Price}
+            </div></div>);
         return (<div className="ProductTileController">
-            <p>ProductTile</p>
-        </div>)
-    }
-}
+       {productRender}
+                </div>
+            )}
+      
+        }
 
-export default ProductTile
+
+    export default ProductTile
