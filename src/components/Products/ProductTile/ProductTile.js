9863325dd6ef1c3ed58be88b10d831e5f52f1c33
omiearicent <39560167+omiearicent@users.noreply.github.com>
2019-05-21 17:24:09 +0530

    Dev og product dash board implemented (#2)
    * Basic file structuring implemented with a default dashboard page.
    
    * Json and ProductImage Structuring.
    
    * Product tile template implemented with dynamic data passed by json file.
    
    * some styling change and method formation for producttile rendering.
    
    * Review comment incorporated.

--- src/components/Products/ProductTile/ProductTile.js
+++ src/components/Products/ProductTile/ProductTile.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 import './ProductTile.scss'
+import axios from 'axios';
+import ProductRender from './ProductRender';
+
 class ProductTile extends Component {
-    
+
+    constructor() {
+        super();
+        this.state = {
+            products: []
+        }
+    }
+
+    componentWillMount() {
+        axios.get('/res/products.json')
+            .then(res => {
+                const products = res.data;
+                console.log(products)
+                this.setState({ products });
+            })
+    }
+
     render() {
         return (<div className={this.props.customClass}>
-            <p>ProductTile</p>
+            <ProductRender products={this.state.products} />
         </div>)
     }
 }
