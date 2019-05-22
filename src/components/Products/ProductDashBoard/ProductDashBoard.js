44c2796676d8621639e891d36998ff24b2db38c3
Omanshu <omanshu.gupta@aricent.com>
2019-05-22 15:26:56 +0530

    Product render functinality is moved to ProductTile.

--- src/components/Products/ProductDashBoard/ProductDashBoard.js
+++ src/components/Products/ProductDashBoard/ProductDashBoard.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 import './ProductDashBoard.scss'
 import ProductTile from '../ProductTile/ProductTile';
+import axios from 'axios'
 
 class ProductDashBoard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            products: []
+        }
+    }
+
+    componentWillMount() {
+        axios.get('/res/products.json')
+            .then(res => {
+                const products = res.data;
+                console.log(products)
+                this.setState({ products });
+            })
+    }
 
     render() {
         return (<div className="main-container">
             <header>
 
             </header>
-            <ProductTile customClass="product-tile-container"/>
+            <ProductTile products={this.state.products} customClass="product-tile-container" />
             <footer>
             </footer>
         </div>);
