25ede3e29da4749953a2d511c069c85df1465afe
omiearicent <39560167+omiearicent@users.noreply.github.com>
2019-05-27 12:37:33 +0530

    change product dash board structuring and added pagination on product dash board. (#3)
    * Product render functinality is moved to ProductTile.
    
    * >Implemented Pagination
    >Modified file structuring

--- src/components/Products/ProductTile/ProductTile.js
+++ src/components/Products/ProductTile/ProductTile.js
@@ -1,31 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './ProductTile.scss'
-import axios from 'axios';
-import ProductRender from './ProductRender';
 
-class ProductTile extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            products: []
-        }
+function ProductTile(props) {
+    let ProductRender = (products) => {
+        return products.map(product => <div className="product"><a href="#">
+            <img src={product.ImgUrl} alt="" width="100" height="100" /></a>
+            <div className="product-description">
+                       Name : {product.Name}
+                <br />Brand : {product.Brand}
+                <br />Price : {product.Price}
+            </div></div>);
     }
+    
+    return (<div className={props.customClass}>
+        {ProductRender(props.products)}
+    </div>)
 
-    componentWillMount() {
-        axios.get('/res/products.json')
-            .then(res => {
-                const products = res.data;
-                console.log(products)
-                this.setState({ products });
-            })
-    }
-
-    render() {
-        return (<div className={this.props.customClass}>
-            <ProductRender products={this.state.products} />
-        </div>)
-    }
 }
 
 export default ProductTile
