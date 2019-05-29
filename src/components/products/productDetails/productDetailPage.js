80de07e8d414516146fb7c776599e5b1df35aaa6
Omanshu <omanshu.gupta@aricent.com>
2019-05-29 14:59:29 +0530

    renaming productDetailPage.

---
+++ src/components/products/productDetails/productDetailPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class ProductDetailPage extends Component {
+
+    render() {
+        return <p>ProductDetailPage</p>
+    }
+}
+
+export default ProductDetailPage
