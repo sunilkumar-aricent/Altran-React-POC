df0f9acd82113d88097da35ad20c855839fd6eae
Omanshu <omanshu.gupta@aricent.com>
2019-05-29 14:52:47 +0530

    Remove duplicate files.

--- src/components/Login/LoginPage.js
+++
@@ -1,7 +0,0 @@
-import React,{Component} from 'react'
-
-class LoginPage extends Component{
-
-}
-
-export default LoginPage
--- src/constants/Constants.js
+++
@@ -1,4 +0,0 @@
-export const PAGE_RANGE = 2;
-export const ITEMS_PER_PAGE = 12;
-export const INITIAL_PAGE = 1;
-
